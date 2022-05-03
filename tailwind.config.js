9cb74e9dab0c77a4af391e21e5bba349dfc3ab60
Jean Victor <jeanvictor.ti@outlook.com>
2022-05-02 23:23:03 -0300

    nlw aula 01

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.tsx"],
+  theme: {
+    extend: {
+      colors: {
+        brand:{
+          500: '#8b5cf6'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
